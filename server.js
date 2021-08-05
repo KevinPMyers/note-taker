1054229854ebc09796d4da2f9f66c8cef40904bc
KevinPMyers <kmyers25@gmail.com>
2021-08-05 19:06:58 -0400

    note page should now load

--- server.js
+++ server.js
@@ -21,7 +21,21 @@ app.use(express.json());
 
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/note.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        
+    })
+})
 
 
 
